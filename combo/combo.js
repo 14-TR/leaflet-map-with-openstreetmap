17e699841ed7a0d6b5fdee854fd611b02d8831c7
TR <58125826+14-TR@users.noreply.github.com>
2024-10-04 23:19:53 -0600

    Update combo.js

--- combo/combo.js
+++ combo/combo.js
@@ -1,18 +1,15 @@
 var map = L.map('combomap').setView([38, -95], 4);
 
-// Add CartoDB DarkMatter basemap
 var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 20
 }).addTo(map);
 
-// Create layer groups
 var weatherAlertsLayer = L.layerGroup();
 var earthquakeLayer = L.layerGroup();
 var radarLayer = L.layerGroup();
 
-// Fetch and add weather alerts data
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
     L.geoJSON(data, {
@@ -29,16 +26,14 @@ $.getJSON(weatherAlertsUrl, function(data) {
     }).addTo(weatherAlertsLayer);
 });
 
-// Add radar layer
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
     layers: 'nexrad-n0r-900913',
     format: 'image/png',
     transparent: true
 };
-var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(radarLayer); // Add to radarLayer
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(radarLayer); 
 
-// Fetch and add earthquake data
 var earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 $.getJSON(earthquakeUrl, function(data) {
     L.geoJSON(data, {
@@ -63,7 +58,6 @@ $.getJSON(earthquakeUrl, function(data) {
     }).addTo(earthquakeLayer);
 });
 
-// Function to determine marker color based on magnitude
 function getColor(magnitude) {
     return magnitude > 6 ? 'magenta' :
            magnitude > 5 ? 'red' :
@@ -74,11 +68,10 @@ function getColor(magnitude) {
                            'lightblue';
 }
 
-// Toggle control
 var overlayMaps = {
     "Weather Alerts": weatherAlertsLayer,
     "Earthquakes": earthquakeLayer,
-    "Radar": radarLayer // Added radarLayer to the toggle
+    "Radar": radarLayer 
 };
 
 L.control.layers(null, overlayMaps, {collapsed: false}).addTo(map);
